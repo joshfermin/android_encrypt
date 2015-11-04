54edf7a880e5235c9df3af1f3724f69c301d1467
joshfermin <joshfermin@gmail.com>
2015-11-04 11:45:51 -0700

    Listing files from sdCard working on frontend

--- app/src/main/java/webroot/com/webrootencrypt/ListFiles.java
+++ app/src/main/java/webroot/com/webrootencrypt/ListFiles.java
@@ -1,14 +1,18 @@
-
-
 package webroot.com.webrootencrypt;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class ListFiles extends AppCompatActivity {
 
@@ -16,6 +20,18 @@ public class ListFiles extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_files);
+
+        ListView sdCard;
+        ArrayList<String> FilesInFolder = GetFiles(Environment.getExternalStorageDirectory().toString());
+
+        sdCard = (ListView)findViewById(R.id.listView);
+        sdCard.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, FilesInFolder));
+
+//        sdCard.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+//                // Clicking on items
+//            }
+//        });
 
         Button button = (Button) findViewById(R.id.goToEncrypt);
 
@@ -48,5 +64,21 @@ public class ListFiles extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public ArrayList<String> GetFiles(String DirectoryPath) {
+        ArrayList<String> MyFiles = new ArrayList<>();
+        File f = new File(DirectoryPath);
+
+        f.mkdirs();
+        File[] files = f.listFiles();
+        if (files.length == 0)
+            return null;
+        else {
+            for (int i=0; i<files.length; i++)
+                MyFiles.add(files[i].getName());
+        }
+
+        return MyFiles;
     }
 }
